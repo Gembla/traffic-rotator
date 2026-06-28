30455308632c50eb6bf3343e2966b287df08e172
Gembla <info@gembla.info>
2026-06-28 14:32:42 +0300

    Edit README

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ use Gembla\TrafficRotator\Rotator;
 $rotator = new Rotator();
 
 // Загружаем ссылки рефовода (например, на разные слоты или казино)
-$rotator->addOffer('https://casino-1.com', 70); // 70% трафика пойдет сюда
-$rotator->addOffer('https://casino-2.com', 30);   // 30% трафика пойдет сюда
+$rotator->addOffer('https://casino-brand-a.com', 70); // 70% трафика пойдет сюда
+$rotator->addOffer('https://casino-brand-b.com', 30);   // 30% трафика пойдет сюда
 
 // Перенаправляем пользователя
 $rotator->redirect();
